8a5cc8043866e447cf0a31e3dcab2a8512961334
Omar <omar.dlsg@gmail.com>
2018-03-07 22:31:45 -0800

    Style buttons

--- assets/javascript/bundle.js
+++ assets/javascript/bundle.js
@@ -244,47 +244,6 @@ var Board = function () {
     return Board;
 }();
 
-// exports.checkBoard = () => {
-// console.log('checking the board');
-// }
-
-// function setupBoard (board) {
-//     board.forEach((num, idx) => {
-//         populateTile(num, idx);
-//     })
-// }
-
-// function createBoards(diff = 'easy') {
-//     const diffMap = {
-//         'easy': 45,
-//         'medium': 35,
-//         'hard': 25
-//     }
-
-//     let solvedBoard = []; 
-
-//     for (let i = 0; i < 81; i++) {
-//         solvedBoard.push(0);
-//     }
-
-//     let j = diffMap[diff];  
-//     solvedBoard.forEach((num, idx) => {
-//         // Math.ceil(Math.random() * 10)
-//         // check against row 
-//         const rowIndex = Math.floor(idx / 9);
-//         const rowStart = rowIndex * 9;
-//         const rowEnd = rowStart + 9;
-//         for (let i = rowStart; i < rowEnd; i++) {
-//             let placed = false; 
-//         }
-
-
-//         // check against column 
-//     })
-
-// } 
-
-
 exports.default = Board;
 
 /***/ }),
